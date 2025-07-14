ed6125f986e622cae5e43f288a524963943ff4e4
José Quintinno <josequintino@hotmail.com.br>
2025-07-14 10:27:47 -0300

    init: Versão Inicial do Projeto

---
+++ src/test/java/br/com/quintinno/detectumapi/DetectumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.quintinno.detectumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
